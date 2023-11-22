8cd3499570a70f7251e0f5877a83ab16ea7dcade
efrosinina <e.efrosininaa@gmail.com>
2023-11-22 15:03:41 +0300

    mistakes correction

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,28 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        Util.getConnection();
+        UserService user = new UserServiceImpl();
+
+        user.createUsersTable();
+
+        user.saveUser("Name1", "LastName1", (byte) 20);
+        System.out.println(user.toString());
+        user.saveUser("Name2", "LastName2", (byte) 25);
+        System.out.println(user);
+        user.saveUser("Name3", "LastName3", (byte) 31);
+        System.out.println(user);
+        user.saveUser("Name4", "LastName4", (byte) 38);
+        System.out.println(user);
+
+        user.removeUserById(1);
+        user.getAllUsers();
+        user.cleanUsersTable();
+        user.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/users";
+    static final String DB_USER = "root";
+    static final String DB_PASSWORD = "LizaWizard77@";
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            System.out.println("Connection is success");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection is failed");
+        }
+        return connection;
+    }
 }
