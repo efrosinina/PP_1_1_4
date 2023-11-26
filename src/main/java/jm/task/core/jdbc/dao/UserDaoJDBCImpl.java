1a1df30020bcac123c6a57997fe61abd25c9fe16
efrosinina <e.efrosininaa@gmail.com>
2023-11-26 16:02:09 +0300

    comment task with using Hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -10,76 +10,75 @@ import java.util.List;
 import jm.task.core.jdbc.util.Util;
 
 public class UserDaoJDBCImpl implements UserDao {
-    //private Connection connection = Util.getConnection();
+    private Connection connection = Util.getConnection();
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-//        String query = "create table if not exists users (id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, name VARCHAR(64) NOT NULL, lastName VARCHAR(64) NOT NULL, age TINYINT)";
-//        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-//            preparedStatement.execute();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
+        String query = "create table if not exists users (id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, name VARCHAR(64) NOT NULL, lastName VARCHAR(64) NOT NULL, age TINYINT)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-//        String query = "drop table if exists users";
-//        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-//            preparedStatement.execute();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
+        String query = "drop table if exists users";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-//        String query = "insert into users (name, lastname, age) VALUES(?, ?, ?)";
-//        try (PreparedStatement  preparedStatement = connection.prepareStatement(query)) {
-//            preparedStatement.setString(1, name);
-//            preparedStatement.setString(2, lastName);
-//            preparedStatement.setByte(3, age);
-//            preparedStatement.executeUpdate();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
+        String query = "insert into users (name, lastname, age) VALUES(?, ?, ?)";
+        try (PreparedStatement  preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-//        String query = "delete from users where id=" + id;
-//        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-//            preparedStatement.executeUpdate();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
+        String query = "delete from users where id=" + id;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-//        String query = "select * from users";
-//        List<User> usersList = new ArrayList<>();
-//        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-//            ResultSet resultSet = preparedStatement.executeQuery(query);
-//            while (resultSet.next()) {
-//                User user = new User();
-//                user.setId(resultSet.getLong("id"));
-//                user.setName(resultSet.getString("name"));
-//                user.setLastName(resultSet.getString("lastName"));
-//                user.setAge(resultSet.getByte("age"));
-//                usersList.add(user);
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return usersList;
-        return null;
+        String query = "select * from users";
+        List<User> usersList = new ArrayList<>();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            ResultSet resultSet = preparedStatement.executeQuery(query);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                usersList.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return usersList;
     }
 
     public void cleanUsersTable() {
-//        String query = "truncate table users";
-//        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-//            preparedStatement.execute();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
+        String query = "truncate table users";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
